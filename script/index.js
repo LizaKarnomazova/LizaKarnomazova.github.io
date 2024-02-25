399797c434eddbe4f79c4e78a628542a45a32744
Liza <karnomazova.liza@yandex.ru>
2024-02-26 00:48:45 +0400

    swiper_mobile

--- script/index.js
+++ script/index.js
@@ -1,14 +1,31 @@
-let companyNavigation = document.querySelector('.services-navigation__list');
-let buttonReadMore = document.querySelector('.button-open');
-let buttonClose = document.querySelector('.button-close');
-buttonReadMore.addEventListener('click', function () {
-    companyNavigation.classList.remove('navigation__list--hide');
-    buttonClose.classList.remove('button__hide');
-    buttonReadMore.classList.add('button__hide');
-});
+/* button */
 
-buttonClose.addEventListener('click', function () {
-    companyNavigation.classList.add('navigation__list--hide');
-    buttonClose.classList.add('button__hide');
-    buttonReadMore.classList.remove('button__hide');
-});
+let companyNavigation = document.querySelector('.services-navigation__list');
+let buttonReadMore = document.querySelector('.button-readMore');
+buttonReadMore.textContent = 'Показать всё';
+    
+buttonReadMore.addEventListener('click', function () {
+    if (buttonReadMore.textContent === 'Показать всё') {
+        companyNavigation.classList.remove('navigation__list--hide');
+        buttonReadMore.classList.add('button-close');
+        buttonReadMore.textContent = 'Скрыть';
+    }
+    else {
+        companyNavigation.classList.add('navigation__list--hide');
+        buttonReadMore.classList.remove('button-close');
+        buttonReadMore.textContent = 'Показать всё';
+    }
+ });
+
+/* swiper */
+
+if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+    var mySwiper = new Swiper('.swiper-container', {
+        loop: true,
+        noSwiping: false,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+}
