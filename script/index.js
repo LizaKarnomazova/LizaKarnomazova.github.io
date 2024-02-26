77ac86316dfea19898b5900050cdf60516f6fc08
Liza <karnomazova.liza@yandex.ru>
2024-02-26 14:42:26 +0400

    MobileHorizontalVersion

--- script/index.js
+++ script/index.js
@@ -1,17 +1,20 @@
 /* button */
 
-let companyNavigation = document.querySelector('.services-navigation__list');
-let buttonReadMore = document.querySelector('.button-readMore');
+let servicesMain = document.querySelector('.services__main');
+let servicesNavigation = servicesMain.querySelector('.services-navigation__list');
+let servicesItem = servicesNavigation.querySelectorAll('.services-navigation__item');
+let buttonReadMore = servicesMain.querySelector('.button-readMore');
+
 buttonReadMore.textContent = 'Показать всё';
     
 buttonReadMore.addEventListener('click', function () {
     if (buttonReadMore.textContent === 'Показать всё') {
-        companyNavigation.classList.remove('navigation__list--hide');
+        servicesNavigation.classList.remove('navigation__list--hide');
         buttonReadMore.classList.add('button-close');
         buttonReadMore.textContent = 'Скрыть';
     }
     else {
-        companyNavigation.classList.add('navigation__list--hide');
+        servicesNavigation.classList.add('navigation__list--hide');
         buttonReadMore.classList.remove('button-close');
         buttonReadMore.textContent = 'Показать всё';
     }
@@ -20,11 +23,19 @@ buttonReadMore.addEventListener('click', function () {
 /* swiper */
 
 if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+    servicesNavigation.classList.add('services-navigation__list--mobile');
+    servicesNavigation.classList.remove('navigation__list--hide');
+    buttonReadMore.classList.add('element--hide');
+
+    for (let i = 0; i < servicesItem.length; i++) {
+        servicesItem[i].classList.add('services-navigation__item--width');
+    }
+
     var mySwiper = new Swiper('.swiper-container', {
         loop: true,
         noSwiping: false,
         slidesPerView: 'auto',
-        spaceBetween: 15,
+        spaceBetween: 35,
         pagination: {
             el: '.swiper-pagination',
             clickable: true,
